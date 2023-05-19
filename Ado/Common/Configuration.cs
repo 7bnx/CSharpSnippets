5e9d2fb1c157ae5f343d62f919a6d9d288754874
7bnx <agreement90@mail.ru>
2023-05-19 21:14:45 +0300

    feat(ado): add common project

---
+++ Ado/Common/Configuration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+  public static class Configuration
+  {
+    private const string SettingsFileName = "appsettings.json";
+    static Configuration()
+    {
+      var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile(SettingsFileName, true, true);
+      var config = builder.Build();
+    }
+
+  }
+}
