9836d41947f16ea8fa4b15743c1067760de0d882
7bnx <agreement90@mail.ru>
2023-05-22 21:54:16 +0300

    feat(ado): add parameters project

--- Ado/Parameters/App.cs
+++ Ado/Parameters/App.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CSharpSnippets.Ado.Common;
+using CSharpSnippets.Ado.Init;
+using CSharpSnippets.Ado.Parameters;
+
+Init init = new();
+init.Drop();
+init.Create();
+init.Insert();
+
+
+Parameters param = new();
+
+Console.WriteLine("Legal query with sql parameters");
+var queryResult = param.FilterWithParameters(
+  "Users",
+  "age",
+  "25",
+  WhereComparison.GREATER,
+  "idUser", "firstName", "age");
+Console.WriteLine($"Rows count: {queryResult.Count}");
+foreach (var k in queryResult)
+  Console.WriteLine($"\t- {new User(k)}");
+
+Console.WriteLine();
+Console.WriteLine("Illegal query with sql injection");
+queryResult = param.FilterWithSQLInjection(
+  "Users", 
+  "age", 
+  "25 UNION SELECT idUser, Phone, idPhone FROM Phones", 
+  WhereComparison.GREATER, 
+  "idUser", "firstName", "age");
+Console.WriteLine($"Rows count: {queryResult.Count}");
+foreach (var k in queryResult)
+  Console.WriteLine($"\t- {new User(k)}");
+
+Console.WriteLine();
+Console.WriteLine("Illegal query with sql parameters");
+queryResult = param.FilterWithParameters(
+  "Users",
+  "age",
+  "25 UNION SELECT idUser, Phone, idPhone FROM Phones",
+  WhereComparison.GREATER,
+  "idUser", "firstName", "age");
+Console.WriteLine($"Rows count: {queryResult.Count}");
+foreach (var k in queryResult)
+  Console.WriteLine($"\t- {new User(k)}");
---
+++ Ado/Parameters/Parameters.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CSharpSnippets.Ado.Common;
+using Microsoft.Data.Sqlite;
+
+namespace CSharpSnippets.Ado.Parameters
+{
+  public class Parameters
+  {
+    public string ConnectionString { get; init; }
+    private static Dictionary<WhereComparison, string> _whereComparison = new()
+    { 
+      {WhereComparison.EUQAL, "=" },
+      {WhereComparison.NOT_EUQAL, "<>" },
+      {WhereComparison.GREATER_OR_EQALS, ">=" },
+      {WhereComparison.GREATER, ">" },
+      {WhereComparison.LESS_OR_EQALS, "<=" },
+      {WhereComparison.LESS, "<" }
+    };
+    public Parameters()
+    {
+      ConnectionString = Configuration.SqLiteConnectionString;
+    }
+
+    private List<List<object>> Filter(
+      bool withInjection,
+      string tableName, 
+      string filterColumn, 
+      object value,
+      WhereComparison comparison,
+      params string[] columns)
+    {
+      List<List<object>> list = new();
+      using SqliteConnection connection = new(ConnectionString);
+      using SqliteCommand command = connection.CreateCommand();
+      try
+      {
+        connection.Open();
+        if (!withInjection)
+        {
+          command.CommandText = $"SELECT {string.Join(',', columns)} FROM {tableName} WHERE {filterColumn} {_whereComparison[comparison]} @value";
+          command.Parameters.AddWithValue("@value", value);
+        }else
+          command.CommandText = $"SELECT {string.Join(',', columns)} FROM {tableName} WHERE {filterColumn} {_whereComparison[comparison]} {value}";
+        using var reader = command.ExecuteReader();
+        if(reader.HasRows)
+        {
+          while(reader.Read())
+          {
+            List<object> row = new();
+            for (int i = 0; i < columns.Length; i++)
+              row.Add(reader[i]);
+            list.Add(row);
+          }
+        }
+      }catch (SqliteException ex)
+      {
+
+      }
+      return list;
+    }
+    public List<List<object>> FilterWithSQLInjection(
+      string tableName,
+      string filterColumn,
+      object value,
+      WhereComparison comparison,
+      params string[] columns)
+    {
+      return Filter(true, tableName, filterColumn, value, comparison, columns);
+    }
+    public List<List<object>> FilterWithParameters(
+      string tableName,
+      string filterColumn,
+      object value,
+      WhereComparison comparison,
+      params string[] columns)
+    {
+      return Filter(false, tableName, filterColumn, value, comparison, columns);
+    }
+  }
+  public enum WhereComparison
+  {
+    EUQAL,
+    NOT_EUQAL,
+    GREATER_OR_EQALS,
+    GREATER,
+    LESS_OR_EQALS,
+    LESS
+  }
+}
