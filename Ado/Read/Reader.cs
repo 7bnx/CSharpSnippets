f07834598b1af294a739bbc0a062e3a9c13b7a17
7bnx <agreement90@mail.ru>
2023-05-22 11:26:29 +0300

    feat(ado): add read project

---
+++ Ado/Read/Reader.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CSharpSnippets.Ado.Common;
+using Microsoft.Data.Sqlite;
+
+namespace CSharpSnippets.Ado.Read
+{
+  public class Reader
+  {
+    public string ConnectionString { get; init; }
+
+    public Reader()
+    {
+      ConnectionString = Configuration.SqLiteConnectionString;
+    }
+
+
+
+  }
+}
