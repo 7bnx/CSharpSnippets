d2b41cc6a6e4caa58231cb83527239d85046a83d
Semyon Ivanov <agreement90@mail.ru>
2023-05-25 13:20:19 +0300

    feat(dependencyinversion): add microsoft di project

---
+++ DependencyInversion/MicrosoftDependencyInjection/ColoredConsoleLoggerService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpSnippets.DependencyInversion.MicrosoftDependencyInjection
+{
+  public class ColoredConsoleLoggerService : ILoggerService
+  {
+    public void Write(string message)
+    {
+      Console.ForegroundColor = ConsoleColor.Green;
+      Console.WriteLine(message);
+      Console.ForegroundColor = ConsoleColor.White;
+    }
+  }
+}
---
+++ DependencyInversion/MicrosoftDependencyInjection/Logger.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpSnippets.DependencyInversion.MicrosoftDependencyInjection
+{
+  public class Logger
+  {
+    ILoggerService? _loggerService;
+    int _loggerId;
+    static int _loggersCount = 0;
+    public Logger(ILoggerService? loggerService)
+      => (_loggerService, _loggerId) = (loggerService, ++_loggersCount);
+    public void Log(string message) 
+      => _loggerService?.Write($"{message}\t| Logger id: {_loggerId}");
+  }
+}
