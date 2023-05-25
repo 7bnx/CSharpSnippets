9d75fc7afd654194bc941d5a17dcb5433404ac20
7bnx <agreement90@mail.ru>
2023-05-25 13:18:18 +0300

    feat(dependencyinversion): add microsoft di project

--- DependencyInversion/MicrosoftDependencyInjection/App.cs
+++ DependencyInversion/MicrosoftDependencyInjection/App.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CSharpSnippets.DependencyInversion.MicrosoftDependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+
+
+Console.WriteLine("Transient");
+var serviceTransient = new ServiceCollection()
+    .AddTransient<ILoggerService, ColoredConsoleLoggerService>()
+    .AddTransient<Logger>();
+var providerTransient = serviceTransient.BuildServiceProvider();
+Output(providerTransient);
+Console.WriteLine();
+
+Console.WriteLine("Scoped");
+var serviceScoped = new ServiceCollection()
+    .AddScoped<ILoggerService, ColoredConsoleLoggerService>()
+    .AddScoped<Logger>();
+using var providerScoped = serviceScoped.BuildServiceProvider();
+Console.WriteLine("First approach");
+Output(providerScoped);
+Console.WriteLine("Second approach");
+Output(providerScoped);
+Console.WriteLine();
+
+Console.WriteLine("Singleton");
+var serviceSingleton = new ServiceCollection()
+    .AddSingleton<ILoggerService, ColoredConsoleLoggerService>()
+    .AddSingleton<Logger>();
+using var providerSingleton = serviceSingleton.BuildServiceProvider();
+Console.WriteLine("First approach");
+Output(providerSingleton);
+Console.WriteLine("Second approach");
+Output(providerSingleton);
+Console.WriteLine();
+
+static void Output(ServiceProvider provider)
+{
+  using var scope = provider.CreateScope();
+  var loggerFirst = scope.ServiceProvider.GetService<Logger>()!;
+  var loggerSecond = scope.ServiceProvider.GetService<Logger>()!;
+  loggerFirst.Log("LoggerFirst");
+  loggerSecond.Log("LoggerSecond");
+}
