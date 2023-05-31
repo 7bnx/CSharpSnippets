3535809782aff8d55581a374718be7144b5fa7a0
7bnx <agreement90@mail.ru>
2023-05-31 23:05:12 +0300

    feat(efcore): override Book.ToString

--- EFCore/Common/Entities/Book.cs
+++ EFCore/Common/Entities/Book.cs
@@ -1,4 +1,6 @@
-﻿namespace CSharpSnippets.EFCore.Common.Entities
+﻿using System.Text;
+
+namespace CSharpSnippets.EFCore.Common.Entities
 {
   public class Book
   {
@@ -16,6 +18,41 @@
     public ICollection<Tag> Tags { get; set; } = null!;
 
     public ICollection<BookAuthor> AuthorsLink { get; set; } = null!;
+    public override string ToString()
+    {
+      StringBuilder sb = new();
+      sb.Append($"Book '{Title}'{Environment.NewLine}");
+      sb.Append($"\tId: {BookId}{Environment.NewLine}");
+      sb.Append($"\tDescription: {Description}{Environment.NewLine}");
+      sb.Append($"\tPublished on {PublishedOn} by {Publisher}{Environment.NewLine}");
+      sb.Append($"\tRegular price: {Price}{Environment.NewLine}");
+      sb.Append($"\tUrl: {Url}{Environment.NewLine}");
+      
+      sb.Append($"\tPromotion{Environment.NewLine}");
+      if (Promotion is not null)
+      {
+        sb.Append($"\t\tOffer: {Promotion.Text}{Environment.NewLine}");
+        sb.Append($"\t\tNew price: {Promotion.NewPrice}{Environment.NewLine}");
+      } else
+        sb.Append("Promotion is null or empty");
+      
+      sb.Append($"\tTags{Environment.NewLine}");
+      if (Tags is not null && Tags.Count > 0)
+      {
+        foreach (var tag in Tags)
+          sb.Append($"\t\t{tag.TagId}{Environment.NewLine}");
+      } else
+        sb.Append("Tags is null or empty");
+
+      sb.Append($"\tAuthors{Environment.NewLine}");
+      if (AuthorsLink is not null && AuthorsLink.Count > 0)
+      {
+        foreach (var author in AuthorsLink)
+          sb.Append($"\t\t{author.Author.Name}{Environment.NewLine}");
+      }else
+        sb.Append("Authors is null or empty");
+      return sb.ToString();
+    }
 
   }
 }
