9577e4520928c7fe0af58b03beffaeb7ac8587af
7bnx <agreement90@mail.ru>
2023-06-01 08:51:45 +0300

    feat(efcore): add common context

---
+++ EFCore/Common/CommonContext.cs
@@ -0,0 +1,30 @@
+﻿using CSharpSnippets.EFCore.Common.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpSnippets.EFCore.Common
+{
+  public class CommonContext : DbContext
+  {
+    public DbSet<Book> Books { get; set; }
+    public DbSet<Author> Authors { get; set; }
+    public DbSet<PriceOffer> PriceOffers { get; set; }
+    public DbSet<Tag> Tags { get; set; }
+
+    public bool EnsureDeleted() => Database.EnsureDeleted();
+    public bool EnsureCreated() => Database.EnsureCreated();
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      optionsBuilder.UseSqlite("Data Source=entity.db");
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+      modelBuilder.Entity<BookAuthor>().HasKey(x => new { x.BookId, x.AuthorId });
+    }
+  }
+}
--- EFCore/Insert/Context.cs
+++ EFCore/Insert/Context.cs
@@ -1,4 +1,5 @@
-﻿using CSharpSnippets.EFCore.Common.Entities;
+﻿using CSharpSnippets.EFCore.Common;
+using CSharpSnippets.EFCore.Common.Entities;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -8,24 +9,12 @@ using System.Threading.Tasks;
 
 namespace CSharpSnippets.EFCore.Insert
 {
-  public class Context : DbContext
+  public class Context : CommonContext
   {
-    public DbSet<Book> Books { get; set; }
-    public DbSet<Author> Authors { get; set; }
-    public DbSet<PriceOffer> PriceOffers { get; set; }
-    public DbSet<Tag> Tags { get; set; }
     public Context()
     {
-      Database.EnsureDeleted();
-      Database.EnsureCreated();
-    }
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-      optionsBuilder.UseSqlite("Data Source=entity.db");
-    }
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-      modelBuilder.Entity<BookAuthor>().HasKey(x => new { x.BookId, x.AuthorId });
+      base.EnsureDeleted();
+      base.EnsureCreated();
     }
   }
 }
