fa7946f88279627190299bcf5938621a83892f32
7bnx <agreement90@mail.ru>
2023-06-16 21:52:14 +0300

    feat(efcore): add create project

---
+++ EFCore/CRUD/Create/App.cs
@@ -0,0 +1,41 @@
+﻿using CSharpSnippets.EFCore.Common.Entities;
+using CSharpSnippets.EFCore.Create;
+using CSharpSnippets.EFCore.Insert;
+
+const string dbName = "Create";
+
+Insert insert = new(dbName);
+insert.DeleteDBAndInsert();
+
+using(var context = new CreateContext(dbName))
+{
+  var book = new Book()
+  {
+    Title = "Book To Create",
+    Description = "Description",
+    PublishedOn = DateTime.Today,
+    Publisher = "Publisher",
+    Url = "URL",
+    Reviews = new List<Review>()
+    {
+      new Review()
+      {
+        NumStars = 3,
+        Text = "Comment Text 1",
+        Reviewer = "Name1"
+      },
+      new Review()
+      {
+        NumStars = 3,
+        Text = "Comment Text 2",
+        Reviewer = "Name2"
+      }
+    }
+  };
+  Console.WriteLine($"State before add: {context.Entry(book).State}");
+  var tt = context.Books.Add(book);
+  Console.WriteLine($"State after add: {context.Entry(book).State}");
+  var createdCount = context.SaveChanges();
+  Console.WriteLine($"State save changes: {context.Entry(book).State}");
+  Console.WriteLine($"Entities added: {createdCount}");
+}
---
+++ EFCore/CRUD/Create/CreateContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpSnippets.EFCore.Create;
+
+public class CreateContext : Common.CommonContext
+{
+  public CreateContext(string dbNama) : base(dbNama) { }
+
+  protected override void OnConfiguringHook(DbContextOptionsBuilder optionsBuilder)
+  {
+    optionsBuilder.LogTo(Console.WriteLine, new[] { RelationalEventId.CommandExecuted });
+  }
+}
