2ec6b25563d30ae2cad6e2b9b4376f8ccd4b7c9e
7bnx <agreement90@mail.ru>
2023-06-16 15:11:20 +0300

    feat(efcore): add client server evaluation project

---
+++ ClientServerEvaluation/ClientServerEvaluationContext.cs
@@ -0,0 +1,15 @@
+﻿using CSharpSnippets.EFCore.Common;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
+namespace CSharpSnippets.EFCore.ClientServerEvaluation;
+
+public class ClientServerEvaluationContext : CommonContext
+{
+  public ClientServerEvaluationContext(string dbNama) : base(dbNama) { }
+
+  protected override void OnConfiguringHook(DbContextOptionsBuilder optionsBuilder)
+  {
+    optionsBuilder.LogTo(Console.WriteLine, new[] { RelationalEventId.CommandExecuted });
+  }
+}
