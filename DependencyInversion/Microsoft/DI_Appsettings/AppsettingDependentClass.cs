8a2f7298587e36efcd002a0e865248e5c7396394
7bnx <agreement90@mail.ru>
2023-06-21 07:19:01 +0300

    feat(microsoftdi): add appsettings project

---
+++ DependencyInversion/Microsoft/DI_Appsettings/AppsettingDependentClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpSnippets.DependencyInversion.Microsoft;
+
+public interface IAppsettingDependentClass
+{
+  void Write();
+}
+
+public class AppsettingDependentDebug : IAppsettingDependentClass
+{
+  public void Write() => Console.WriteLine(nameof(AppsettingDependentDebug));
+}
+
+public class AppsettingDependentRelease : IAppsettingDependentClass
+{
+  public void Write() => Console.WriteLine(nameof(AppsettingDependentRelease));
+}
