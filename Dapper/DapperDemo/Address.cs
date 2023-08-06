c403d90e95f22b6f6748adfe89c621194a666764
7bnx <agreement90@mail.ru>
2023-08-06 20:05:55 +0300

    feat: add dapper demo

---
+++ Dapper/DapperDemo/Address.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpSnippets.Dapper.Demo;
+public record Address
+{
+  public int AddressId { get; set; }
+  public string Name { get; set; } = null!;
+}
---
+++ Dapper/DapperDemo/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpSnippets.Dapper.Demo;
+public record User
+{
+  public int UserId { get; set; }
+  public string Name { get; set; } = null!;
+  public int Age { get; set; }
+
+  public int AddressId { get; set; }
+  public Address? Address { get; set; }
+}
